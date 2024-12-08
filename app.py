921dffb284a7670f094c1285113ec47b17e71784
Daniel Salin <daniel.salin@gmail.com>
2024-12-08 14:06:34 +0100

    Add playground file for matplotlib

---
+++ app.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+
+numbers = [1,2,4,8,16,32]
+more_numbers = [1,2,3,4,5,6]
+plt.plot(numbers)
+plt.ylabel('some numbers')
+plt.xlabel('steady interval')
+plt.plot(more_numbers)
+plt.show()
